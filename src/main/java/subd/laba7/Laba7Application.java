4d44bcf0a89749730249f8f56888b01096c01ed1
Demid5 <demkozlov@yandex.ru>
2020-01-11 19:16:51 +0700

    add connection with DB

--- src/main/java/subd/laba7/Laba7Application.java
+++ src/main/java/subd/laba7/Laba7Application.java
@@ -3,18 +3,12 @@ package subd.laba7;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.SQLException;
+
 @SpringBootApplication
 public class Laba7Application {
     // application
     public static void main(String[] args) {
-
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-            return;
-        }
-
         SpringApplication.run(Laba7Application.class, args);
     }
 
