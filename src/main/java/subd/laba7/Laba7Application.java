7e5bc0dedc9c55b2c7a5baaa228abf472f303624
demid5 <demkozlov@yandex.ru>
2020-01-11 18:23:39 +0700

    first commit

--- src/main/java/subd/laba7/Laba7Application.java
+++ src/main/java/subd/laba7/Laba7Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Laba7Application {
-
+    // application
     public static void main(String[] args) {
 
         try {
