9e276176a6b4403b9e93e6cf8002a33f1641fff3
Mikhail Rusakov <rusmih98@yandex.ru>
2020-01-13 10:36:57 +0700

    можно принимать товар

--- src/main/java/subd/laba7/controllers/ConsultantController.java
+++ src/main/java/subd/laba7/controllers/ConsultantController.java
@@ -9,9 +9,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import subd.laba7.database.BDConnection;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.Random;
 
 @Controller
 @Slf4j
@@ -49,9 +48,51 @@ public class ConsultantController {
 
         try {
             String pkTov, pkClient;
-            Statement statement = connection.createStatement();
+            PreparedStatement statement = connection.prepareStatement("select insert_client(?, ?)");
+            statement.setString(1, fio);
+            statement.setString(2, number);
+            statement.execute();
+            Statement statement1 = connection.createStatement();
+            ResultSet resultSet = statement1.executeQuery("select last_value from \"Client_PK_client_seq\";");
+            resultSet.next();
+            pkClient = resultSet.getString("last_value");
 
-            ResultSet resultSet = statement.executeQuery("select \"Client_PK_client_seq\".nextval");
+            statement = connection.prepareStatement("insert into \"Tovar\"(\"PK_tovar\", \"Naim\", \"Data_nachala_garantii\", \"Zavodsokoi_nomer\", \"Prodavez\", \"PK_status_tovar\") " +
+                    "values(DEFAULT, ?, ?, ?, ?, 1)");
+            statement.setString(1, naim);
+            statement.setDate(2, Date.valueOf(dataProd));
+            statement.setString(3, zavNumber);
+            statement.setString(4, prod);
+            statement.execute();
+
+            resultSet = statement1.executeQuery("select last_value from \"Tovar_PK_tovar_seq\";");
+            resultSet.next();
+            pkTov = resultSet.getString("last_value");
+
+            String aktNumber = String.valueOf(Math.abs((new Random()).nextInt()));
+
+            statement = connection.prepareStatement
+                    ("insert into \"Zayavlenie_na_rem_tovar\"(\"PK_zayvlene_na_removt\", \"Nomer\", \"Data_oformlenya\", \"PK_client\", \"PK_kosultant\", \"PK_tovar\") " +
+                    "values (DEFAULT, ?, (select now()), ?, ?, ?);");
+            statement.setString(1, aktNumber);
+            statement.setInt(2, Integer.parseInt(pkClient));
+            statement.setInt(3, Integer.parseInt(pk));
+            statement.setInt(4, Integer.parseInt(pkTov));
+            statement.execute();
+
+            statement = connection.prepareStatement
+                    ("insert into \"Akt_o_prinyatia_tovar\"(\"PK_akt_o_prin_tovar\", \"Nomer\", \"Poln_inform_o_izdel\", \"Kontaktn_dann_zayavitela\", \"Psportnye_dann_zayavitela\", \"Data_prodazhi\", \"Opisanie_sost_tovar\", \"Podrobn_opisan_obl_probl\", \"PK_kosultant\", \"PK_tovar\") " +
+                    "values (DEFAULT, ?, ?, ?, ?, ?, ?, ?, ?, ?);");
+            statement.setString(1, aktNumber);
+            statement.setString(2, inf);
+            statement.setString(3, number);
+            statement.setString(4, passport);
+            statement.setDate(5, Date.valueOf(dataProd));
+            statement.setString(6, sost);
+            statement.setString(7, problem);
+            statement.setInt(8, Integer.parseInt(pk));
+            statement.setInt(9, Integer.parseInt(pkTov));
+            statement.execute();
 
         }
         catch (Exception e) {
